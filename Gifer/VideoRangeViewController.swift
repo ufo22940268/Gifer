c5a2fba005dcab2ef57ade3ff71d98719379333f
Frank Cheng <ufo22940268@gmail.com>
2019-03-19 17:06:34 +0800

    v

--- Gifer/VideoRangeViewController.swift
+++ Gifer/VideoRangeViewController.swift
@@ -21,7 +21,9 @@ class VideoRangeViewController: UIViewController {
     var currentItem: AVPlayerItem {
         return player.currentItem!
     }
+    @IBOutlet weak var videoController: VideoController!
     var previewAsset: PHAsset!
+    var timeObserverToken: Any?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,13 +38,39 @@ class VideoRangeViewController: UIViewController {
         options.isNetworkAccessAllowed = true
         options.deliveryMode = .automatic
         PHImageManager.default().requestPlayerItem(forVideo: phAsset, options: options) { (playerItem, _) in
+            guard let playerItem = playerItem else { return }
             DispatchQueue.main.async {
                 //TODO
                 self.previewController.player = AVPlayer(playerItem: playerItem)
-                self.previewController.player!.addObserver(self, forKeyPath: #keyPath(AVPlayerItem.status), options: [.old, .new], context: nil)
                 self.previewController.player?.play()
                 self.previewController.view.translatesAutoresizingMaskIntoConstraints = false
+                self.videoController.load(playerItem: playerItem, completion: {
+                    
+                })
+                self.registerObservers()
             }
+        }
+    }
+    
+    private func registerObservers() {
+        self.previewController.player!.addObserver(self, forKeyPath: #keyPath(AVPlayerItem.status), options: [.old, .new], context: nil)
+        timeObserverToken = player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 0.1, preferredTimescale: 600),
+                                                            queue: .main) {
+                                                                [weak self] time in
+                                                                self?.observePlayProgress(progress: time)
+        }
+    }
+    
+    private func observePlayProgress(progress: CMTime) {
+        if case AVPlayer.Status.readyToPlay = player.status {
+            videoController.updateSliderProgress(progress)
+        }
+    }
+    
+    private func unregisterObservers() {
+        self.removeObserver(self.previewController.player!, forKeyPath: #keyPath(AVPlayerItem.status))
+        if let timeObserverToken = timeObserverToken {
+            player.removeTimeObserver(timeObserverToken)
         }
     }
     
@@ -58,7 +86,7 @@ class VideoRangeViewController: UIViewController {
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        self.removeObserver(self.previewController.player!, forKeyPath: #keyPath(AVPlayerItem.status))
+        unregisterObservers()
     }
     
     
