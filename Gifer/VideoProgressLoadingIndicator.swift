0f5230034abc1da627013c08c4c17e6c7668477b
Frank Cheng <ufo22940268@gmail.com>
2019-03-20 20:19:42 +0800

    v

--- Gifer/VideoProgressLoadingIndicator.swift
+++ Gifer/VideoProgressLoadingIndicator.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class VideoProgressLoadingIndicator: UIStackView {
+class VideoProgressLoadingIndicator: UIVisualEffectView {
 
     lazy var messageView: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = tintColor
+        label.backgroundColor = .clear
         return label
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let view = UIStackView()
+        view.isLayoutMarginsRelativeArrangement = true
+        view.layoutMargins = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .horizontal
+        return view
     }()
     
     override func awakeFromNib() {
@@ -22,11 +32,20 @@ class VideoProgressLoadingIndicator: UIStackView {
     }
     
     private func setup() {
-        axis = .horizontal
+        layer.cornerRadius = 8
+        clipsToBounds = true
+        effect = UIBlurEffect(style: .extraLight)
         tintColor = UIColor.darkGray
+        
+        contentView.addSubview(stackView)
 
-        addArrangedSubview(messageView)
+        stackView.addArrangedSubview(messageView)
         messageView.text = "正在下载视频"
         messageView.sizeToFit()
+        
+        NSLayoutConstraint.activate([
+            widthAnchor.constraint(equalTo: stackView.widthAnchor),
+            heightAnchor.constraint(equalTo: stackView.heightAnchor)
+            ])
     }
 }
