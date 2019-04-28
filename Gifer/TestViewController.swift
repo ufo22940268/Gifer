58bf25d8e2feb10d7c176bbb4233ec11181a6b2f
Frank Cheng <ufo22940268@gmail.com>
2019-04-28 15:14:28 +0800

    Refactor folder structure

--- Gifer/TestViewController.swift
+++ Gifer/TestViewController.swift
@@ -10,28 +10,15 @@ import UIKit
 
 class TestViewController: UIViewController {
     
-//    @IBOutlet weak var textEditFrame: EditTextFrame!
-    var originViewHeight: CGFloat?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         
-        // Do any additional setup after loading the view.
+        print("test")
+        showTestDialog()
     }
     
-    @objc func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-            originViewHeight = self.view.frame.height
-            self.view.frame.size.height -= keyboardSize.height
-        }
+    func showTestDialog() {
+        
     }
-    
-    @objc func keyboardWillHide(notification: NSNotification) {
-        if let originViewHeight = originViewHeight {
-            self.view.frame.size.height = originViewHeight
-        }
-    }
-    
 }
+
