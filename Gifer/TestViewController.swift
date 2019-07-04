f765053e0a5df03a2475bbb056e7fecc74c62d33
Frank Cheng <ufo22940268@gmail.com>
2019-07-04 10:46:35 +0800

    v

--- Gifer/TestViewController.swift
+++ Gifer/TestViewController.swift
@@ -7,16 +7,31 @@
 //
 
 import UIKit
+import Photos
 
 class TestViewController: UIViewController {
-    
-    @IBOutlet weak var p: VideoProgressLoadingIndicator!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            self.p.progress = 0.4
-            self.p.progress = 0.8
+        
+        var livePhotos = [PHAsset]()
+        let assets = PHAsset.fetchAssets(with: .image, options: nil)
+        assets.enumerateObjects { (asset, _, _) in
+            if asset.mediaSubtypes == .photoLive {
+                livePhotos.append(asset)
+            }
+        }
+        
+        let livePhoto = livePhotos.first!
+        print(livePhoto)
+        PHImageManager.default().requestLivePhoto(for: livePhoto, targetSize: CGSize(width: 500, height: 500), contentMode: .aspectFit, options: nil) { (photo, info) in
+            let url = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent("livePhoto")
+            try! FileManager.default.removeItem(at: url)
+            let options = PHAssetResourceRequestOptions()
+            options.isNetworkAccessAllowed = true
+            PHAssetResourceManager.default().writeData(for: PHAssetResource.assetResources(for: livePhoto).first { $0.type == PHAssetResourceType.pairedVideo }!, toFile: url, options: options, completionHandler: { (error) in
+                
+                let asset = AVAsset(url: url)
+            })
         }
     }
 }
