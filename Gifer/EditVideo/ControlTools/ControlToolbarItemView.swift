2f4f8b6d64811d62fa7c50b2d00a3201f5ef2048
Frank Cheng <ufo22940268@gmail.com>
2019-07-07 14:02:30 +0800

    v

--- Gifer/EditVideo/ControlTools/ControlToolbarItemView.swift
+++ Gifer/EditVideo/ControlTools/ControlToolbarItemView.swift
@@ -8,19 +8,6 @@
 
 import UIKit
 
-public extension UIButton {
-    
-    func alignTextUnderImage(spacing: CGFloat = 6.0) {
-        if let image = self.imageView?.image
-        {
-            let imageSize: CGSize = image.size
-            self.titleEdgeInsets = UIEdgeInsets(top: spacing, left: -imageSize.width, bottom: -(imageSize.height), right: 0.0)
-            let labelString = NSString(string: self.titleLabel!.text!)
-            let titleSize = labelString.size(withAttributes: [NSAttributedString.Key.font: self.titleLabel!.font!])
-            self.imageEdgeInsets = UIEdgeInsets(top: -(titleSize.height + spacing), left: 0.0, bottom: 0.0, right: -titleSize.width)
-        }
-    }
-}
 
 class ControlToolbarItemView: UICollectionViewCell {
 
--- Gifer/Helper/BasicExtensions.swift
+++ Gifer/Helper/BasicExtensions.swift
@@ -52,11 +52,15 @@ extension UIView {
 }
 
 extension UIButton {
-    override open var intrinsicContentSize: CGSize {
-        let intrinsicContentSize = super.intrinsicContentSize
-        let adjustedWidth = intrinsicContentSize.width + titleEdgeInsets.left + titleEdgeInsets.right
-        let adjustedHeight = intrinsicContentSize.height + titleEdgeInsets.top + titleEdgeInsets.bottom
-        return CGSize(width: adjustedWidth, height: adjustedHeight)
+    func alignTextUnderImage(spacing: CGFloat = 6.0) {
+        if let image = self.imageView?.image
+        {
+            let imageSize: CGSize = image.size
+            self.titleEdgeInsets = UIEdgeInsets(top: spacing, left: -imageSize.width, bottom: -(imageSize.height), right: 0.0)
+            let labelString = NSString(string: self.titleLabel!.text!)
+            let titleSize = labelString.size(withAttributes: [NSAttributedString.Key.font: self.titleLabel!.font!])
+            self.imageEdgeInsets = UIEdgeInsets(top: -(titleSize.height + spacing), left: 0.0, bottom: 0.0, right: -titleSize.width)
+        }
     }
 }
 
