b4d0efb950cc5c46a1d720e8a0f4419cfa88e360
Frank Cheng <ufo22940268@gmail.com>
2019-07-11 06:10:35 +0800

    v

--- Gifer/EditVideo/CropViewController.swift
+++ Gifer/EditVideo/CropViewController.swift
@@ -51,14 +51,11 @@ class CropViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    
+    @IBAction func onDone(_ sender: Any) {
+    }    
 }
